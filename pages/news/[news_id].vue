4ed03059fac7fc6c77250e8bc4eb283ce5b4658f
Rynare <rynare.of.legend@gmail.com>
2024-10-28 23:16:35 +0700

    membuat page news

---
+++ pages/news/[news_id].vue
@@ -0,0 +1,152 @@
+<template>
+  <div>
+    <section
+      id="section0"
+      class="bg-cover bg-center w-full bg-blend md:h-[330px] h-[200px] flex justify-center items-end md:items-center"
+    >
+      <div class="text-center">
+        <h2 class="text-thin font-nunito text-[14px] text-black">SUB-TITLE</h2>
+        <h1 class="text-size-main font-times text-[#38544c]">
+          Fahim Developer Handal: Dapat Mengoding Realita
+        </h1>
+      </div>
+    </section>
+    <section
+      id="section1"
+      class="bg-[#F8F4EC] w-full flex justify-center pt-12 md:pt-0"
+    >
+      <div class="w-full max-w-5xl mx-auto">
+        <div class="flex items-center justify-center w-full px-2 pb-4">
+          <div class="flex flex-col md:flex-row">
+            <div class="mx-auto">
+              <div
+                class="flex items-center justify-between mb-6 text-sm text-gray-500 font-nunito"
+              >
+                <span class="italic">
+                  Published on: <strong>12 Oktober 2024</strong>
+                </span>
+              </div>
+              <div class="mx-auto mb-8 md:mb-0">
+                <img
+                  src="/img/fahimDev.jpg"
+                  alt="Gambar Utama Artikel"
+                  class="object-cover w-full h-auto"
+                />
+              </div>
+
+              <div
+                class="py-8 mx-auto text-sm leading-relaxed text-justify text-gray-700 font-nunito"
+              >
+                <p>
+                  Fahim adalah seorang developer yang dikenal karena
+                  kemampuannya yang luar biasa dalam memahami kode dan
+                  mengubahnya menjadi solusi nyata yang berdampak bagi kehidupan
+                  banyak orang. Di era digital yang penuh perubahan ini, peran
+                  developer menjadi lebih signifikan, dan Fahim membuktikan
+                  dirinya sebagai sosok yang tak hanya terampil dalam aspek
+                  teknis, tetapi juga memiliki visi besar dalam mengubah dunia
+                  melalui teknologi.
+                  <br />
+                  <br />
+                  Dengan latar belakang yang solid di berbagai bahasa
+                  pemrograman seperti JavaScript, Python, dan PHP, Fahim mampu
+                  merancang aplikasi dan sistem yang tak hanya berfungsi dengan
+                  baik tetapi juga mampu menangani skala besar dengan stabilitas
+                  yang tinggi. Proyek-proyeknya mencakup berbagai sektor, mulai
+                  dari aplikasi bisnis hingga aplikasi sosial yang bermanfaat
+                  bagi komunitas. Salah satu kunci kesuksesan Fahim adalah
+                  kemampuannya dalam menggabungkan kecerdasan buatan (AI), big
+                  data, dan cloud computing ke dalam proyek-proyeknya, membuat
+                  solusi yang ia hasilkan tidak hanya efektif tetapi juga
+                  berkelanjutan.
+                  <br /><br />
+                  Fahim memiliki moto "mengoding realita" yang mengisyaratkan
+                  bagaimana ia menggunakan keterampilan teknisnya untuk
+                  menciptakan solusi praktis bagi masalah nyata. Ini terbukti
+                  dalam salah satu proyek besarnya, yaitu sebuah platform
+                  kesehatan digital yang mampu menghubungkan pasien dengan
+                  layanan medis secara langsung, memungkinkan akses ke perawatan
+                  yang cepat dan mudah. Dengan platform ini, Fahim telah
+                  membantu ratusan pasien yang sebelumnya kesulitan mendapatkan
+                  layanan medis di daerah terpencil.
+                  <br />
+                  <br />
+                  Saat berbicara mengenai karyanya, Fahim selalu menekankan
+                  pentingnya inovasi yang berpusat pada pengguna. Ia percaya
+                  bahwa teknologi seharusnya tidak membuat hidup menjadi lebih
+                  rumit tetapi justru mempermudah, dan filosofi inilah yang
+                  terlihat dalam setiap proyek yang ia kerjakan. Misalnya, dalam
+                  mengembangkan aplikasi-aplikasi produktivitas, Fahim selalu
+                  berusaha agar aplikasinya memiliki antarmuka yang intuitif,
+                  ramah pengguna, dan hemat sumber daya sehingga dapat digunakan
+                  oleh siapa saja tanpa memerlukan perangkat mahal.
+                  <br />
+                  <br />
+                  Perjalanan Fahim sebagai developer dimulai dari
+                  ketertarikannya pada teknologi sejak usia muda. Dengan kerja
+                  keras dan dedikasinya, ia berhasil menjadi developer
+                  profesional yang tidak hanya bekerja untuk
+                  perusahaan-perusahaan besar, tetapi juga berkontribusi kepada
+                  komunitas lokal melalui workshop dan pelatihan. Fahim dikenal
+                  sebagai sosok mentor yang tidak pelit ilmu, selalu siap
+                  membantu para developer muda untuk memulai karir mereka di
+                  dunia pemrograman.
+                  <br />
+                  <br />
+                  Tidak berhenti di situ, Fahim berencana untuk terus
+                  mengembangkan proyek-proyek yang berdampak positif bagi
+                  masyarakat. Salah satu visinya adalah menciptakan platform
+                  edukasi berbasis online yang dapat diakses gratis oleh
+                  masyarakat di daerah terpencil, yang bertujuan untuk
+                  memberdayakan mereka dengan keterampilan digital. Menurut
+                  Fahim, teknologi memiliki kekuatan untuk menyamakan
+                  kesempatan, dan ia berkomitmen untuk mewujudkannya.
+                  <br />
+                  <br />
+                  Fahim juga percaya bahwa developer masa kini harus memiliki
+                  pemahaman yang mendalam tentang tanggung jawab etis, terutama
+                  terkait privasi data dan keberlanjutan. Ia aktif dalam
+                  berbagai forum dan komunitas developer, di mana ia berbagi
+                  wawasan tentang pentingnya membangun aplikasi yang menjaga
+                  privasi pengguna dan mengurangi dampak lingkungan. Bagi Fahim,
+                  teknologi bukan hanya soal bisnis, tetapi juga soal bagaimana
+                  membuat dunia menjadi tempat yang lebih baik.
+                  <br />
+                  <br />
+                  Dengan dedikasi dan semangatnya, Fahim terus berinovasi dan
+                  memberikan inspirasi bagi banyak orang. Tidak hanya sekadar
+                  membuat kode, Fahim telah membuktikan bahwa melalui teknologi,
+                  ia bisa mengubah realita dan memberikan manfaat nyata bagi
+                  masyarakat. Dalam beberapa tahun ke depan, kontribusi Fahim
+                  dalam dunia teknologi diharapkan akan terus berkembang, dan ia
+                  siap untuk menghadapi tantangan baru dalam mewujudkan visinya
+                  tentang dunia yang lebih terhubung dan inklusif melalui
+                  teknologi.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<style scoped>
+.bg-blend {
+  background: linear-gradient(
+      to top,
+      rgb(248, 244, 236),
+      rgba(248, 244, 236, 0.9),
+      rgba(248, 244, 236, 0.715),
+      rgba(248, 244, 236, 0.486),
+      rgba(248, 244, 236, 0.486),
+      rgba(248, 244, 236, 0.486),
+      rgba(248, 244, 236, 0.486),
+      rgba(245, 245, 233, 0),
+      rgba(245, 245, 233, 0),
+      rgba(245, 245, 233, 0)
+    ),
+    url("/img/bGNews.png");
+  background-size: cover;
+}
+</style>
---
+++ pages/news/index.vue
@@ -0,0 +1,353 @@
+<template>
+  <div>
+    <section
+      id="section0"
+      class="bg-cover bg-center w-full bg-blend md:h-[330px] h-[200px] flex justify-center items-end md:items-center"
+    >
+      <div class="text-center">
+        <h2 class="text-thin font-nunito text-[14px] text-black">
+          NEWS & EVENTS
+        </h2>
+        <h1 class="text-size-main font-times text-[#38544c]">
+          Community Information at GreenLand
+        </h1>
+      </div>
+    </section>
+
+    <section
+      id="section1"
+      class="bg-[#F8F4EC] w-full justify-center pt-12 md:pt-0"
+    >
+      <div class="w-full">
+        <div class="flex items-center justify-center w-full px-2 pb-4">
+          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  GreenLand antisipasi MPOX dengan POS Kesehatan
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="100"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="200"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center w-full px-2 pb-4">
+          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  GreenLand antisipasi MPOX dengan POS Kesehatan
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="100"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="200"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center w-full px-2 pb-4">
+          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  GreenLand antisipasi MPOX dengan POS Kesehatan
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="100"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="200"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center w-full px-2 pb-4">
+          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  GreenLand antisipasi MPOX dengan POS Kesehatan
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="100"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="200"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center w-full px-2 pb-4">
+          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  GreenLand antisipasi MPOX dengan POS Kesehatan
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="100"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="200"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="pt-4 pb-8 text-center">
+          <button
+            class="flex items-center pt-5 mx-auto btn-visit-black"
+            data-aos="fade-up"
+            data-aos-delay="300"
+          >
+            <span class="text-xs tracking-wider text">VIEW ALL</span>
+            <span class="circle">
+              <span class="arrow">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-arrow-right"
+                  viewBox="0 0 24 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 8a.5.5 0 0 1 .5-.5h18.793l-3.146-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L20.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  />
+                  <path d="M18 8l2-2v4l-2-2z" fill="currentColor" />
+                </svg>
+              </span>
+            </span>
+          </button>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<style scoped>
+.bg-blend {
+  background: linear-gradient(
+      to top,
+      rgb(248, 244, 236),
+      rgba(248, 244, 236, 0.9),
+      rgba(248, 244, 236, 0.715),
+      rgba(248, 244, 236, 0.486),
+      rgba(248, 244, 236, 0.486),
+      rgba(248, 244, 236, 0.486),
+      rgba(248, 244, 236, 0.486),
+      rgba(245, 245, 233, 0),
+      rgba(245, 245, 233, 0),
+      rgba(245, 245, 233, 0)
+    ),
+    url("/img/bGNews.png");
+  background-size: cover;
+}
+</style>
