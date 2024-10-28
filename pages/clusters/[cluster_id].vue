ffe0a07a45b6abe42f18fea97cf777c74a9af042
Rynare <rynare.of.legend@gmail.com>
2024-10-28 23:16:14 +0700

    membuat page cluster

---
+++ pages/clusters/[cluster_id].vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <section
+      id="section0"
+      class="items-end justify-center block w-full bg-center bg-cover bg-blend h-fit"
+    >
+      <div class="pt-48 text-center" data-aos="slide-up">
+        <h2 class="text-thin font-nunito text-[14px] text-black">
+          GREENLAND CLUSTERS
+        </h2>
+        <h1 class="text-size-main font-times text-[#38544c]">
+          Garbera Clusters
+        </h1>
+      </div>
+
+      <div class="flex-row block gap-8 px-16 pt-12 md:flex">
+        <div class="md:w-1/2 w-full bg-[#38544c]" data-aos="fade-down">
+          <div
+            class="flex items-center justify-between px-4 pt-48 pb-3 text-white md:pt-64"
+          >
+            <span class="text-lg font-times">Facade</span>
+            <p class="text-[10px] font-nunito">VIEW GALLERY</p>
+          </div>
+        </div>
+        <div
+          class="md:w-1/2 w-full bg-[#38544c] md:mt-0 mt-4"
+          data-aos="fade-down"
+        >
+          <div
+            class="flex items-center justify-between px-12 pt-48 pb-3 text-white md:pt-64"
+          >
+            <span class="text-lg font-times">Interior</span>
+            <p class="text-[10px] font-nunito">VIEW GALLERY</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="section1" class="bg-[#F8F4EC]">
+      <div class="flex-row justify-between block px-16 pt-10 md:flex">
+        <div class="w-full md:w-3/4">
+          <p class="font-bold text-[#38544c] pb-6" data-aos="fade-up">
+            GARBERA CLUSTER<span class="font-thin"> CONCPET</span>
+          </p>
+          <p
+            class="md:w-[80%] w-full leading-[25px] text-[12px] font-[490]"
+            data-aos="fade-up"
+          >
+            Garbera cluster di Greenland menawarkan hunian praktis dengan luas
+            80m2 yang dirancang secara harmonis dan memberikan kesan lapang
+            serta multi fungsi. Penghuni Garbera cluster adalah mereka yang
+            memiliki jiwa aktif dalam kesehariannya, yakni seorang eksekutif
+            atau pengusaha muda yang mempersiapkan masa depannya untuk
+            berekspansi hingga menjadi keluarga kecil dengan 1-2 orang anak.
+            <br />
+            <br />
+            Bentuk facade yang menggunakan warna unsur kayu sangat menyatu
+            dengan lingkungan Greenland yang mengedepankan kehidupan alam hijau.
+            Dengan guarded gate cluster, hunian di Garbera cluster sangat aman
+            meski tanpa dilengkapi oleh pagar. Selain itu kehidupan bersosial
+            dengan sesama dapat terjalin dengan baik sembari tetap memiliki
+            privasi yang eksklusif.
+          </p>
+        </div>
+        <div class="w-full mt-8 md:w-1/4 md:mt-0">
+          <p class="font-thin text-[#38544c] md:pb-6 pb-8" data-aos="fade-down">
+            FLOOR PLAN & CLUSTER LAYOUT
+          </p>
+          <ul class="text-xs font-bold text-[#38544c]" data-aos="fade-down">
+            <li class="flex mb-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="#000"
+                class="bi bi-filetype-pdf me-2"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803q.43 0 .732-.173.305-.175.463-.474a1.4 1.4 0 0 0 .161-.677q0-.375-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333v3.999h1.46q.602 0 .998-.237a1.45 1.45 0 0 0 .595-.689q.196-.45.196-1.084 0-.63-.196-1.075a1.43 1.43 0 0 0-.589-.68q-.396-.234-1.005-.234zm.791.645h.563q.371 0 .609.152a.9.9 0 0 1 .354.454q.118.302.118.753a2.3 2.3 0 0 1-.068.592 1.1 1.1 0 0 1-.196.422.8.8 0 0 1-.334.252 1.3 1.3 0 0 1-.483.082h-.563zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638z"
+                />
+              </svg>
+              Garbera Floor Plan
+            </li>
+            <li class="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="#000"
+                class="bi bi-filetype-pdf me-2"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803q.43 0 .732-.173.305-.175.463-.474a1.4 1.4 0 0 0 .161-.677q0-.375-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333v3.999h1.46q.602 0 .998-.237a1.45 1.45 0 0 0 .595-.689q.196-.45.196-1.084 0-.63-.196-1.075a1.43 1.43 0 0 0-.589-.68q-.396-.234-1.005-.234zm.791.645h.563q.371 0 .609.152a.9.9 0 0 1 .354.454q.118.302.118.753a2.3 2.3 0 0 1-.068.592 1.1 1.1 0 0 1-.196.422.8.8 0 0 1-.334.252 1.3 1.3 0 0 1-.483.082h-.563zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638z"
+                />
+              </svg>
+              Garbera Floor Map
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="flex justify-end px-16 py-28">
+        <div class="text-center">
+          <button
+            class="flex items-center pt-5 mx-auto btn-visit-black"
+            data-aos="fade-up"
+            data-aos-delay="300"
+          >
+            <span class="text-xs tracking-wider text">BACK TO CLUSTERS</span>
+            <span class="circle">
+              <span class="arrow">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-arrow-right"
+                  viewBox="0 0 24 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 8a.5.5 0 0 1 .5-.5h18.793l-3.146-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L20.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  />
+                  <path d="M18 8l2-2v4l-2-2z" fill="currentColor" />
+                </svg>
+              </span>
+            </span>
+          </button>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<style scoped>
+.bg-blend {
+  background: linear-gradient(
+      to top,
+      rgba(248, 244, 236, 0.9),
+      rgba(248, 244, 236, 0.9),
+      rgba(248, 244, 236, 0.8),
+      rgba(248, 244, 236, 0.7),
+      rgba(248, 244, 236, 0.6),
+      rgba(248, 244, 236, 0.6)
+    ),
+    url("/img/detaiLBg.png");
+  background-size: cover;
+}
+</style>
