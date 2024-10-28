095acd597d10815bfac667ee0b0a83d74eda89dc
Rynare <rynare.of.legend@gmail.com>
2024-10-28 23:17:22 +0700

    membuat page promo

---
+++ pages/promo.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <section
+      id="section0"
+      class="bg-cover bg-center w-full bg-blend md:h-[300px] h-[200px] md:pt-40 pt-0 flex justify-center items-end md:items-center"
+    >
+      <div class="text-center">
+        <h2 class="text-thin font-nunito text-[14px] text-black">
+          PROMO & OFFERS
+        </h2>
+        <h1 class="text-size-main font-times text-[#38544c]">
+          Your Green Home Awaits
+        </h1>
+      </div>
+    </section>
+
+    <section
+      id="section1"
+      class="bg-[#F8F4EC] w-full justify-center md:pt-12 pt-8 pb-24"
+    >
+      <div class="w-full">
+        <div class="flex items-center justify-center w-full px-2 pb-4">
+          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  GreenLand antisipasi MPOX dengan POS Kesehatan
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="100"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+            <div
+              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+              data-aos="fade-up"
+              data-aos-duration="300"
+              data-aos-delay="200"
+            >
+              <div>
+                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
+                <h3 class="text-xl font-base leading-[30px] mt-2">
+                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
+                  download
+                </h3>
+              </div>
+              <div class="mt-auto">
+                <hr class="my-3 border-[#BFCEC9FF]" />
+                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<style scoped>
+.bg-blend {
+  background: linear-gradient(
+      to top,
+      rgb(248, 244, 236),
+      rgba(248, 244, 236, 0.9),
+      rgba(248, 244, 236, 0.8),
+      rgba(248, 244, 236, 0.7),
+      rgba(248, 244, 236, 0.6),
+      rgba(248, 244, 236, 0.6)
+    ),
+    url("/img/bGPromo.png");
+  background-size: cover;
+}
+</style>
