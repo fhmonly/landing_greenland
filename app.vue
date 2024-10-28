6cfc239572097aa7ae1a30e1993a0677314ae758
Rynare <rynare.of.legend@gmail.com>
2024-10-28 17:40:03 +0700

    Setup app

--- app.vue
+++ app.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <Body>
+    <NuxtLoadingIndicator color="red" :throttle="100" />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </Body>
 </template>
+<style>
+.no-tailwind * {
+  all: revert;
+}
+
+.font-nunito {
+  font-family: "Nunito Sans", sans-serif;
+}
+
+.font-times {
+  font-family: "Times New Roman", serif;
+}
+</style>
+<script setup>
+useSeoMeta({
+  titleTemplate: (titleChunk) => {
+    return titleChunk
+      ? `${titleChunk} | Greenland Elegant Homes and Investment`
+      : "Greenland Elegant Homes and Investment";
+  },
+});
+useHead({
+  link: [
+    {
+      href: "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600&display=swap",
+      rel: "stylesheet",
+    },
+    {
+      href: "https://fonts.googleapis.com/css2?family=Times+New+Roman&display=swap",
+      rel: "stylesheet",
+    },
+  ],
+});
+</script>
