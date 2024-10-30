69a8aca398a38396cdb290331e3b2d7999f78c1b
Rynare <rynare.of.legend@gmail.com>
2024-10-30 11:22:20 +0700

    integrasi api kedalam page news

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,353 +1,67 @@
 <template>
   <div>
-    <section
-      id="section0"
-      class="bg-cover bg-center w-full bg-blend md:h-[330px] h-[200px] flex justify-center items-end md:items-center"
-    >
+    <section id="section0"
+      class="bg-cover bg-center w-full bg-blend md:h-[330px] h-[200px] flex justify-center items-end md:items-center">
       <div class="text-center">
         <h2 class="text-thin font-nunito text-[14px] text-black">
-          NEWS & EVENTS
+          {{ settings.data.hero_news_title_1 }}
         </h2>
         <h1 class="text-size-main font-times text-[#38544c]">
-          Community Information at GreenLand
+          {{ settings.data.hero_news_info_1 }}
         </h1>
       </div>
     </section>
 
-    <section
-      id="section1"
-      class="bg-[#F8F4EC] w-full justify-center pt-12 md:pt-0"
-    >
-      <div class="w-full">
-        <div class="flex items-center justify-center w-full px-2 pb-4">
-          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  GreenLand antisipasi MPOX dengan POS Kesehatan
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-              data-aos-delay="100"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
-                  download
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-              data-aos-delay="200"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
-                  download
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
+    <section id="section1" class="bg-[#F8F4EC] w-full justify-center pt-12 md:pt-0">
+      <div class="flex flex-wrap items-center justify-center w-full gap-4 px-2 pb-4">
+        <div
+          class="bg-white text-[#38544c] w-full max-w-[350px] tablet:max-w-[48%] md:max-w-[40%] lg:max-w-[31%] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
+          data-aos="fade-up" data-aos-duration="300" v-for="item in news.data.data" :key="item.blog_id">
+          <div>
+            <p class="text-[10px] font-semibold">NEWS - {{ $dayjs(item.date_input).locale("id").fromNow() }}</p>
+            <h3 class="text-xl font-base leading-[30px] mt-2">
+              {{ item.title }}
+            </h3>
           </div>
-        </div>
-        <div class="flex items-center justify-center w-full px-2 pb-4">
-          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  GreenLand antisipasi MPOX dengan POS Kesehatan
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-              data-aos-delay="100"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
-                  download
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-              data-aos-delay="200"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
-                  download
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
+          <div class="mt-auto">
+            <hr class="my-3 border-[#BFCEC9FF]" />
+            <NuxtLink :to="`/news/${item.blog_id}`" class="mb-4 text-[10px] font-bold">READ MORE</NuxtLink>
           </div>
-        </div>
-        <div class="flex items-center justify-center w-full px-2 pb-4">
-          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  GreenLand antisipasi MPOX dengan POS Kesehatan
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-              data-aos-delay="100"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
-                  download
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-              data-aos-delay="200"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
-                  download
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="flex items-center justify-center w-full px-2 pb-4">
-          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  GreenLand antisipasi MPOX dengan POS Kesehatan
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-              data-aos-delay="100"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
-                  download
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-              data-aos-delay="200"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
-                  download
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="flex items-center justify-center w-full px-2 pb-4">
-          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 3 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  GreenLand antisipasi MPOX dengan POS Kesehatan
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-              data-aos-delay="100"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
-                  download
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-            <div
-              class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
-              data-aos="fade-up"
-              data-aos-duration="300"
-              data-aos-delay="200"
-            >
-              <div>
-                <p class="text-[10px] font-semibold">NEWS - 5 SEPTEMBER 2024</p>
-                <h3 class="text-xl font-base leading-[30px] mt-2">
-                  Pembaharuan aplikasi P3RS penghuni GreenLand sudah bisa di
-                  download
-                </h3>
-              </div>
-              <div class="mt-auto">
-                <hr class="my-3 border-[#BFCEC9FF]" />
-                <a href="#" class="mb-4 text-[10px] font-bold">READ MORE</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="pt-4 pb-8 text-center">
-          <button
-            class="flex items-center pt-5 mx-auto btn-visit-black"
-            data-aos="fade-up"
-            data-aos-delay="300"
-          >
-            <span class="text-xs tracking-wider text">VIEW ALL</span>
-            <span class="circle">
-              <span class="arrow">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-arrow-right"
-                  viewBox="0 0 24 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M1 8a.5.5 0 0 1 .5-.5h18.793l-3.146-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L20.293 8.5H1.5A.5.5 0 0 1 1 8z"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  />
-                  <path d="M18 8l2-2v4l-2-2z" fill="currentColor" />
-                </svg>
-              </span>
-            </span>
-          </button>
         </div>
       </div>
     </section>
   </div>
 </template>
-<style scoped>
-.bg-blend {
-  background: linear-gradient(
-      to top,
-      rgb(248, 244, 236),
-      rgba(248, 244, 236, 0.9),
-      rgba(248, 244, 236, 0.715),
-      rgba(248, 244, 236, 0.486),
-      rgba(248, 244, 236, 0.486),
-      rgba(248, 244, 236, 0.486),
-      rgba(248, 244, 236, 0.486),
-      rgba(245, 245, 233, 0),
-      rgba(245, 245, 233, 0),
-      rgba(245, 245, 233, 0)
-    ),
-    url("/img/bGNews.png");
-  background-size: cover;
-}
-</style>
+<script setup>
+const settings = useDataSettings()
+const config = useRuntimeConfig()
+const { data: news, status, error, refresh } = await useFetch(`${config.public.apiUrl}/blogs`, {
+  pick: ["data"],
+  key: "api-news-blogs"
+});
+useHead({
+  style: [
+    {
+      innerHTML: `
+        .bg-blend {
+          background: linear-gradient(
+              to top,
+              rgb(248, 244, 236),
+              rgba(248, 244, 236, 0.9),
+              rgba(248, 244, 236, 0.715),
+              rgba(248, 244, 236, 0.486),
+              rgba(248, 244, 236, 0.486),
+              rgba(248, 244, 236, 0.486),
+              rgba(248, 244, 236, 0.486),
+              rgba(245, 245, 233, 0),
+              rgba(245, 245, 233, 0),
+              rgba(245, 245, 233, 0)
+            ),
+            url("${config.public.dirUrl}/images/static/${settings.value.data.hero_news_image_1}");
+          background-size: cover;
+        }
+      `
+    }
+  ]
+});
+</script>
