e949b12e6c7db92ca50eee5a981f6b177b5906a6
Rynare <rynare.of.legend@gmail.com>
2024-10-30 11:20:55 +0700

    integrasi api kedalam page about

--- pages/about.vue
+++ pages/about.vue
@@ -1,24 +1,12 @@
 <template>
   <div>
-    <section
-      id="section1"
-      class="bg-center bg-cover"
-      style="background-image: url('/img/bGSection1.jpg')"
-    >
+    <section id="section1" class="bg-center bg-cover"
+      :style="`background-image: url('${config.public.dirUrl}/images/static/${settings.data.hero_about_image_1}')`">
       <div class="flex flex-row h-screen bg-black bg-opacity-20">
         <div class="flex items-center justify-start flex-grow">
           <div class="px-8 text-left text-white md:px-16">
-            <h1
-              class="text-4xl font-normal leading-tight md:text-5xl text-size-main font-times"
-              data-aos="fade-up"
-            >
-              Building A Lifetime
-              <br />
-              Unforgettable
-              <br />
-              Memories At Your
-              <br />
-              Own Sanctuary
+            <h1 class="text-4xl font-normal leading-tight md:text-5xl text-size-main font-times" data-aos="fade-up">
+              {{ settings.data.hero_about_title_1 }}
             </h1>
           </div>
         </div>
@@ -27,23 +15,12 @@
     <section id="section2" class="bg-[#3c4f4b] h-screen">
       <div class="flex items-center justify-center h-full">
         <div class="block px-16 mx-auto mb-16 md:flex">
-          <div
-            class="w-2/4 text-white font-nunito text-md"
-            data-aos="fade-down"
-          >
-            <span class="font-thin text-[14px]">GREENLAND</span> <br />
-            <span class="font-semibold">YOUR PATH OF SUCCESS</span>
+          <div class="w-2/4 text-white font-nunito text-md" data-aos="fade-down">
+            {{settings.data.about1_small_title}}
           </div>
           <div class="w-3/4 mt-5 md:mt-0" data-aos="fade-up">
-            <p
-              class="text-white text-size-main font-times opacity-60"
-              id="textSection2"
-            >
-              Discover a perfect blend <br />
-              of elegant homes and investment for life beyond luxury, a life
-              filled with <br />
-              joy and prosperity
-            </p>
+            <div class="text-white text-size-main font-times opacity-60 no-tailwind" id="textSection2"
+              v-html="settings.data.about1_title"></div>
           </div>
         </div>
       </div>
@@ -52,125 +29,54 @@
       </h1>
     </section>
 
-    <section
-      id="section3"
-      class="bg-center bg-cover"
-      data-aos="fade-up"
-    ></section>
+    <section id="section3" class="bg-center bg-cover" data-aos="fade-up" :style="`background-image: url('${config.public.dirUrl}/images/static/${settings.data.about1_image_banner}')`">
+    </section>
     <section id="section4" class="bg-[#f5f4ed] h-fit">
       <div class="grid items-center justify-center h-full">
-        <div
-          class="order-2 block px-16 mx-auto mt-6 mb-8 md:flex md:mt-16 md:order-1"
-        >
+        <div class="order-2 block px-16 mx-auto mt-6 mb-8 md:flex md:mt-16 md:order-1">
           <div class="w-2/4 font-nunito text-main text-md" data-aos="zoom-in">
-            <span class="font-thin text-[14px]">ABOUT GREENLAND</span> <br />
+            <span class="font-thin text-[14px]">{{ settings.data.about2_small_title }}</span> <br />
           </div>
           <div class="w-3/4 mt-5 md:mt-0">
-            <p
-              class="text-size-main md:w-[100%] w-[150%] text-main font-times"
-              id="textSection2"
-              data-aos="zoom-in"
-            >
-              Building your Green home is a seamless integrated experience at
-              Greenland
+            <p class="text-size-main md:w-[100%] w-[150%] text-main font-times" id="textSection2" data-aos="zoom-in">
+              {{ settings.data.about2_title }}
             </p>
-            <p
-              class="text-black text-desc md:w-[90%] w-[139%] text-justify font-nunito mt-8"
-              data-aos="fade-up"
-            >
-              Terletak di jantung Surabaya Barat, kawasan yang penuh dengan
-              perkembangan dinamis, Greenland adalah destinasi investasi masa
-              depan. Wilayah ini semakin bergairah dengan derap kemajuan
-              infrastruktur, menjadikannya area yang ideal untuk pertumbuhan
-              keluarga dan aset properti yang bernilai tinggi. Di Greenland,
-              Anda tidak hanya berinvestasi dalam hunian, tetapi juga dalam gaya
-              hidup masa depan yang sehat dan berkelanjutan. <br /><br />
-              Greenland hadir sebagai oasis kehidupan modern yang menyatu dengan
-              alam. Sebuah kawasan hunian yang dirancang dengan prinsip
-              keberlanjutan dan keseimbangan lingkungan, Greenland menawarkan
-              kehidupan yang lebih green—dimana setiap langkah diiringi oleh
-              rimbunnya pepohonan dan sejuknya udara segar. Di sini, Anda tidak
-              hanya menemukan rumah, tetapi juga ruang hidup yang dipenuhi
-              ketenangan dan kedamaian alam, menghadirkan harmoni sejati antara
-              manusia dan lingkungan. <br /><br />
-              Masterplan GreenLand seluas 100 Ha terdiri atas 8 residential
-              cluster dan 80 premium shophouse. Untuk menunjang gaya hidup sehat
-              dan menghadirkan kawasan yang teduh, GreenLand menghadirkan family
-              clubhouse, dan 2 thematic garden: Green Exhibition Park di area
-              komersial, dan Green Line Adventure Park yang berdekatan dengan
-              clubhouse. Fasilitas taman hijau yang luas menjadi jantung
-              Greenland, menawarkan berbagai aktivitas outdoor yang mendukung
-              gaya hidup vibrant dan aktif. Mulai dari jogging, skateboarding,
-              hingga dog walking, penghuni Greenland akan merasakan kebebasan
-              hidup yang penuh energi di setiap sudut kawasan. Setiap hari
-              adalah kesempatan baru untuk bergerak, bersosialisasi, dan
-              menikmati alam terbuka yang asri. Greenland adalah gaya hidup,
-              tempat harmoni alam dan kemajuan bertemu.
-            </p>
+            <div class="text-black text-desc md:w-[90%] w-[139%] text-justify font-nunito mt-8 no-tailwind" data-aos="fade-up" v-html="settings.data.about2_content">
+            </div>
           </div>
         </div>
         <div class="flex order-1 w-full place-items-end md:order-2">
           <div class="md:w-2/4 w-[0%] text-second">.</div>
-          <div
-            class="w-full pl-0 m-5 md:w-3/4 md:pl-3 md:m-0"
-            data-aos="fade-down"
-          >
-            <img
-              src="/img/imgSection4.png"
-              alt=""
-              class="place-items-end"
-              height="10px"
-            />
+          <div class="w-full pl-0 m-5 md:w-3/4 md:pl-3 md:m-0" data-aos="fade-down">
+            <img :src="`${config.public.dirUrl}/images/static/${settings.data.about2_image_banner}`" alt="" class="place-items-end" height="10px" />
           </div>
         </div>
       </div>
     </section>
     <section id="section5" class="bg-[#2c3c34] h-fit">
-      <div
-        class="flex flex-col px-4 pt-12 mx-auto mb-8 md:flex-row md:px-16 md:pt-24 md:mb-16"
-        data-aos="fade-up"
-        data-aos-duration="1000"
-      >
-        <div
-          class="md:w-2/4 w-full font-nunito text-[#f5f4ed] text-md text-center md:text-left"
-        >
-          <span class="font-thin text-[14px]">PROJECT HIGHLIGHT</span> <br />
+      <div class="flex flex-col px-4 pt-12 mx-auto mb-8 md:flex-row md:px-16 md:pt-24 md:mb-16" data-aos="fade-up"
+        data-aos-duration="1000">
+        <div class="md:w-2/4 w-full font-nunito text-[#f5f4ed] text-md text-center md:text-left">
+          <span class="font-thin text-[14px]">{{settings.data.about3_small_title}}</span> <br />
         </div>
         <div class="w-full mt-3 md:w-3/4 md:mt-0">
-          <p
-            class="text-size-main text-center md:text-left text-[#f5f4ed] font-times"
-            id="textSection2"
-            data-aos="fade-up"
-            data-aos-delay="200"
-          >
-            Greenland may serve as either <br />
-            your healing paradise or <br />
-            a centralized living activity
+          <p class="text-size-main text-center md:text-left text-[#f5f4ed] font-times" id="textSection2"
+            data-aos="fade-up" data-aos-delay="200">
+            {{settings.data.about3_title}}
           </p>
         </div>
       </div>
 
-      <div
-        class="flex flex-col gap-6 px-4 pb-12 mt-6 md:flex-row md:px-16 md:mt-12 md:pb-24"
-        data-aos="fade-up"
-        data-aos-duration="1000"
-      >
-        <div
-          class="w-full text-white md:w-1/2"
-          data-aos="zoom-in"
-          data-aos-delay="300"
-        >
+      <div class="flex flex-col gap-6 px-4 pb-12 mt-6 md:flex-row md:px-16 md:mt-12 md:pb-24" data-aos="fade-up"
+        data-aos-duration="1000">
+        <div class="w-full text-white md:w-1/2" data-aos="zoom-in" data-aos-delay="300">
           <img src="/img/about2.png" alt="" class="w-full mx-auto md:w-auto" />
           <p class="mt-3 text-2xl text-left font-times">
             Healing <span class="italic">Paradise</span>
           </p>
           <p class="font-nunito text-[10px] mt-1 text-left">VIEW GALLERY</p>
         </div>
-        <div
-          class="w-full text-white md:w-1/2"
-          data-aos="zoom-in"
-          data-aos-delay="500"
-        >
+        <div class="w-full text-white md:w-1/2" data-aos="zoom-in" data-aos-delay="500">
           <img src="/img/about1.png" alt="" class="w-full mx-auto md:w-auto" />
           <p class="mt-3 text-2xl text-left font-times">
             Everyday <span class="italic">Activities</span>
@@ -185,18 +91,16 @@
 .gradient-text {
   font-family: "Times New Roman", serif;
   font-size: 115px;
-  background: linear-gradient(
-    to top,
-    rgba(255, 255, 255, 1),
-    rgba(255, 255, 255, 0.7),
-    rgba(255, 255, 255, 0.4),
-    rgba(255, 255, 255, 0.2),
-    rgba(255, 255, 255, 0.2),
-    rgba(255, 255, 255, 0.2),
-    rgba(255, 255, 255, 0),
-    rgba(255, 255, 255, 0),
-    rgba(255, 255, 255, 0)
-  );
+  background: linear-gradient(to top,
+      rgba(255, 255, 255, 1),
+      rgba(255, 255, 255, 0.7),
+      rgba(255, 255, 255, 0.4),
+      rgba(255, 255, 255, 0.2),
+      rgba(255, 255, 255, 0.2),
+      rgba(255, 255, 255, 0.2),
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 0));
   -webkit-background-clip: text;
   background-clip: text;
   color: transparent;
@@ -205,7 +109,6 @@
 #section3 {
   height: 1000px;
   width: 100%;
-  background-image: url("/img/bGSection3.png");
 }
 
 .text-desc {
@@ -223,3 +126,7 @@
   }
 }
 </style>
+<script setup>
+const settings = useDataSettings();
+const config = useRuntimeConfig();
+</script>
