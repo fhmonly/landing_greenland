a1243a48884291ef2e7e831f4eb5972952814179
Rynare <rynare.of.legend@gmail.com>
2024-10-31 13:21:58 +0700

    integrasi api di pagenews detail

--- pages/news/[news_id].vue
+++ pages/news/[news_id].vue
@@ -5,9 +5,11 @@
       class="bg-cover bg-center w-full bg-blend md:h-[330px] h-[200px] flex justify-center items-end md:items-center"
     >
       <div class="text-center">
-        <h2 class="text-thin font-nunito text-[14px] text-black">SUB-TITLE</h2>
+        <h2 class="text-thin font-nunito text-[14px] text-black">
+          {{ newsDetail.data[0].topik_id }}
+        </h2>
         <h1 class="text-size-main font-times text-[#38544c]">
-          Fahim Developer Handal: Dapat Mengoding Realita
+          {{ newsDetail.data[0].title }}
         </h1>
       </div>
     </section>
@@ -23,107 +25,26 @@
                 class="flex items-center justify-between mb-6 text-sm text-gray-500 font-nunito"
               >
                 <span class="italic">
-                  Published on: <strong>12 Oktober 2024</strong>
+                  Published on:
+                  <strong>{{
+                    $dayjs(newsDetail.data[0].date_input)
+                      .locale("id")
+                      .format("DD MMMM YYYY")
+                  }}</strong>
                 </span>
               </div>
               <div class="mx-auto mb-8 md:mb-0">
                 <img
-                  src="/img/fahimDev.jpg"
+                  :src="`${config.public.dirUrl}/${newsDetail.data[0].image}`"
                   alt="Gambar Utama Artikel"
                   class="object-cover w-full h-auto"
                 />
               </div>
 
               <div
-                class="py-8 mx-auto text-sm leading-relaxed text-justify text-gray-700 font-nunito"
-              >
-                <p>
-                  Fahim adalah seorang developer yang dikenal karena
-                  kemampuannya yang luar biasa dalam memahami kode dan
-                  mengubahnya menjadi solusi nyata yang berdampak bagi kehidupan
-                  banyak orang. Di era digital yang penuh perubahan ini, peran
-                  developer menjadi lebih signifikan, dan Fahim membuktikan
-                  dirinya sebagai sosok yang tak hanya terampil dalam aspek
-                  teknis, tetapi juga memiliki visi besar dalam mengubah dunia
-                  melalui teknologi.
-                  <br />
-                  <br />
-                  Dengan latar belakang yang solid di berbagai bahasa
-                  pemrograman seperti JavaScript, Python, dan PHP, Fahim mampu
-                  merancang aplikasi dan sistem yang tak hanya berfungsi dengan
-                  baik tetapi juga mampu menangani skala besar dengan stabilitas
-                  yang tinggi. Proyek-proyeknya mencakup berbagai sektor, mulai
-                  dari aplikasi bisnis hingga aplikasi sosial yang bermanfaat
-                  bagi komunitas. Salah satu kunci kesuksesan Fahim adalah
-                  kemampuannya dalam menggabungkan kecerdasan buatan (AI), big
-                  data, dan cloud computing ke dalam proyek-proyeknya, membuat
-                  solusi yang ia hasilkan tidak hanya efektif tetapi juga
-                  berkelanjutan.
-                  <br /><br />
-                  Fahim memiliki moto "mengoding realita" yang mengisyaratkan
-                  bagaimana ia menggunakan keterampilan teknisnya untuk
-                  menciptakan solusi praktis bagi masalah nyata. Ini terbukti
-                  dalam salah satu proyek besarnya, yaitu sebuah platform
-                  kesehatan digital yang mampu menghubungkan pasien dengan
-                  layanan medis secara langsung, memungkinkan akses ke perawatan
-                  yang cepat dan mudah. Dengan platform ini, Fahim telah
-                  membantu ratusan pasien yang sebelumnya kesulitan mendapatkan
-                  layanan medis di daerah terpencil.
-                  <br />
-                  <br />
-                  Saat berbicara mengenai karyanya, Fahim selalu menekankan
-                  pentingnya inovasi yang berpusat pada pengguna. Ia percaya
-                  bahwa teknologi seharusnya tidak membuat hidup menjadi lebih
-                  rumit tetapi justru mempermudah, dan filosofi inilah yang
-                  terlihat dalam setiap proyek yang ia kerjakan. Misalnya, dalam
-                  mengembangkan aplikasi-aplikasi produktivitas, Fahim selalu
-                  berusaha agar aplikasinya memiliki antarmuka yang intuitif,
-                  ramah pengguna, dan hemat sumber daya sehingga dapat digunakan
-                  oleh siapa saja tanpa memerlukan perangkat mahal.
-                  <br />
-                  <br />
-                  Perjalanan Fahim sebagai developer dimulai dari
-                  ketertarikannya pada teknologi sejak usia muda. Dengan kerja
-                  keras dan dedikasinya, ia berhasil menjadi developer
-                  profesional yang tidak hanya bekerja untuk
-                  perusahaan-perusahaan besar, tetapi juga berkontribusi kepada
-                  komunitas lokal melalui workshop dan pelatihan. Fahim dikenal
-                  sebagai sosok mentor yang tidak pelit ilmu, selalu siap
-                  membantu para developer muda untuk memulai karir mereka di
-                  dunia pemrograman.
-                  <br />
-                  <br />
-                  Tidak berhenti di situ, Fahim berencana untuk terus
-                  mengembangkan proyek-proyek yang berdampak positif bagi
-                  masyarakat. Salah satu visinya adalah menciptakan platform
-                  edukasi berbasis online yang dapat diakses gratis oleh
-                  masyarakat di daerah terpencil, yang bertujuan untuk
-                  memberdayakan mereka dengan keterampilan digital. Menurut
-                  Fahim, teknologi memiliki kekuatan untuk menyamakan
-                  kesempatan, dan ia berkomitmen untuk mewujudkannya.
-                  <br />
-                  <br />
-                  Fahim juga percaya bahwa developer masa kini harus memiliki
-                  pemahaman yang mendalam tentang tanggung jawab etis, terutama
-                  terkait privasi data dan keberlanjutan. Ia aktif dalam
-                  berbagai forum dan komunitas developer, di mana ia berbagi
-                  wawasan tentang pentingnya membangun aplikasi yang menjaga
-                  privasi pengguna dan mengurangi dampak lingkungan. Bagi Fahim,
-                  teknologi bukan hanya soal bisnis, tetapi juga soal bagaimana
-                  membuat dunia menjadi tempat yang lebih baik.
-                  <br />
-                  <br />
-                  Dengan dedikasi dan semangatnya, Fahim terus berinovasi dan
-                  memberikan inspirasi bagi banyak orang. Tidak hanya sekadar
-                  membuat kode, Fahim telah membuktikan bahwa melalui teknologi,
-                  ia bisa mengubah realita dan memberikan manfaat nyata bagi
-                  masyarakat. Dalam beberapa tahun ke depan, kontribusi Fahim
-                  dalam dunia teknologi diharapkan akan terus berkembang, dan ia
-                  siap untuk menghadapi tantangan baru dalam mewujudkan visinya
-                  tentang dunia yang lebih terhubung dan inklusif melalui
-                  teknologi.
-                </p>
-              </div>
+                class="py-8 mx-auto text-sm leading-relaxed text-justify text-gray-700 font-nunito no-tailwind"
+                v-html="newsDetail.data[0].content"
+              ></div>
             </div>
           </div>
         </div>
@@ -150,3 +71,15 @@
   background-size: cover;
 }
 </style>
+<script setup>
+const config = useRuntimeConfig();
+const route = useRoute();
+const {
+  data: newsDetail,
+  status,
+  error,
+  refresh,
+} = useFetch(`${config.public.apiUrl}/blog?id=${route.params.news_id}`, {
+  pick: ["data"],
+});
+</script>
