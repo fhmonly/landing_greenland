d6306be4ba48096f88e1a72be06783ae4f8f52e4
Rynare <rynare.of.legend@gmail.com>
2024-10-31 23:17:04 +0700

    update page cluster

--- pages/clusters/index.vue
+++ pages/clusters/index.vue
@@ -38,10 +38,8 @@ useHead({
 </script>
 <template>
   <div>
-    <section
-      id="section0"
-      class="bg-cover bg-center w-full bg-blend md:h-[400px] h-[200px] flex justify-center items-end md:pb-[5rem] pb-6"
-    >
+    <section id="section0"
+      class="bg-cover bg-center w-full bg-blend md:h-[400px] h-[200px] flex justify-center items-end md:pb-[5rem] pb-6">
       <div class="text-center">
         <h2 class="text-thin font-nunito text-[14px] text-black">
           {{ settings.data.hero_cluster_title_1 }}
@@ -52,64 +50,35 @@ useHead({
       </div>
     </section>
 
-    <section
-      id="section1"
-      class="bg-[#F8F4EC] w-full justify-center pt-6 md:pt-0"
-    >
-      <div class="flex flex-wrap gap-4 w-[90%] mx-auto">
-        <div
-          class="relative group w-full sm:max-w-[48%] md:max-w-[31%] lg:max-w-[23%]"
-          data-aos="zoom-in"
-          v-for="cluster in clusters.data.data"
-          :key="cluster.id"
-        >
-          <img
-            :src="`${config.public.dirUrl}/${cluster.image}`"
-            alt="Photo 1"
-            class="object-cover w-full h-auto"
-          />
+    <section id="section1" class="bg-[#F8F4EC] w-full justify-center pt-6 md:pt-0">
+      <div class="flex flex-wrap items-center justify-center gap-4 mx-auto">
+        <div class="relative group w-full sm:max-w-[48%] lg:max-w-[23%]" data-aos="zoom-in"
+          v-for="cluster in clusters.data.data" :key="cluster.id">
+          <img :src="`${config.public.dirUrl}/${cluster.image}`" alt="Photo 1" class="object-cover w-full h-auto" />
           <div
-            class="absolute inset-0 flex flex-col justify-end p-5 text-white transition-all duration-300 bg-black bg-opacity-20 group-hover:bg-white group-hover:bg-opacity-50"
-          >
-            <h3
-              class="text-4xl font-times capitalize group-hover:text-[#38544c] transition-all duration-300 mb-3"
-            >
+            class="absolute inset-0 flex flex-col justify-end p-5 text-white transition-all duration-300 bg-black bg-opacity-20 group-hover:bg-white group-hover:bg-opacity-50">
+            <h3 class="text-4xl font-times capitalize group-hover:text-[#38544c] transition-all duration-300 mb-3">
               {{ cluster.name }}
             </h3>
             <div class="flex items-center justify-start">
-              <NuxtLink
-                :to="`/clusters/${cluster.id}`"
-                class="text-white group-hover:text-[#38544c] transition-all duration-300 text-[10px] font-bold"
-                >VIEW GALLERY</NuxtLink
-              >
+              <NuxtLink :to="`/clusters/${cluster.id}`"
+                class="text-white group-hover:text-[#38544c] transition-all duration-300 text-[10px] font-bold">VIEW
+                GALLERY</NuxtLink>
             </div>
           </div>
         </div>
       </div>
 
       <div class="pt-8 pb-10 text-center">
-        <button
-          class="flex items-center pt-5 mx-auto btn-visit-black"
-          data-aos="fade-up"
-          data-aos-delay="300"
-        >
+        <button class="flex items-center pt-5 mx-auto btn-visit-black" data-aos="fade-up" data-aos-delay="300">
           <span class="text-xs tracking-wider text">SCHEDULE A VISIT</span>
           <span class="circle">
             <span class="arrow">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="16"
-                fill="currentColor"
-                class="bi bi-arrow-right"
-                viewBox="0 0 24 16"
-              >
-                <path
-                  fill-rule="evenodd"
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor"
+                class="bi bi-arrow-right" viewBox="0 0 24 16">
+                <path fill-rule="evenodd"
                   d="M1 8a.5.5 0 0 1 .5-.5h18.793l-3.146-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L20.293 8.5H1.5A.5.5 0 0 1 1 8z"
-                  stroke="currentColor"
-                  stroke-width="2"
-                />
+                  stroke="currentColor" stroke-width="2" />
                 <path d="M18 8l2-2v4l-2-2z" fill="currentColor" />
               </svg>
             </span>
