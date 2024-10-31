63b16e710f23c241de85cb42a27595f961204eee
Rynare <rynare.of.legend@gmail.com>
2024-10-31 15:28:36 +0700

    memperbaiki page cluster dan detail cluster

--- pages/clusters/[cluster_id].vue
+++ pages/clusters/[cluster_id].vue
@@ -1,3 +1,39 @@
+<script setup>
+const route = useRoute();
+const config = useRuntimeConfig();
+const {
+  data: cluster,
+  status,
+  error,
+  refresh,
+} = await useFetch(
+  `${config.public.apiUrl}/cluster?id=${route.params.cluster_id}`,
+  {
+    pick: ["data"],
+  }
+);
+useHead({
+  style: [
+    {
+      innerHTML: `
+        .bg-blend {
+          background: linear-gradient(
+              to top,
+              rgba(248, 244, 236, 0.9),
+              rgba(248, 244, 236, 0.9),
+              rgba(248, 244, 236, 0.8),
+              rgba(248, 244, 236, 0.7),
+              rgba(248, 244, 236, 0.6),
+              rgba(248, 244, 236, 0.6)
+            ),
+            url("${config.public.dirUrl}/${cluster.value.data.image2}");
+          background-size: cover;
+        }
+      `,
+    },
+  ],
+});
+</script>
 <template>
   <div>
     <section
@@ -9,27 +45,22 @@
           GREENLAND CLUSTERS
         </h2>
         <h1 class="text-size-main font-times text-[#38544c]">
-          Garbera Clusters
+          {{ cluster.data.name }}
         </h1>
       </div>
 
-      <div class="flex-row block gap-8 px-16 pt-12 md:flex">
-        <div class="md:w-1/2 w-full bg-[#38544c]" data-aos="fade-down">
+      <div class="flex flex-wrap gap-8 px-16 pt-12">
+        <div
+          class="md:w-[48%] w-full bg-[#38544c] bg-center bg-no-repeat bg-fixed"
+          data-aos="fade-down"
+          v-for="facade in cluster.data.facade"
+          :key="facade.id"
+          :style="`background-image:url('${config.public.dirUrl}/${facade.image}')`"
+        >
           <div
             class="flex items-center justify-between px-4 pt-48 pb-3 text-white md:pt-64"
           >
-            <span class="text-lg font-times">Facade</span>
-            <p class="text-[10px] font-nunito">VIEW GALLERY</p>
-          </div>
-        </div>
-        <div
-          class="md:w-1/2 w-full bg-[#38544c] md:mt-0 mt-4"
-          data-aos="fade-down"
-        >
-          <div
-            class="flex items-center justify-between px-12 pt-48 pb-3 text-white md:pt-64"
-          >
-            <span class="text-lg font-times">Interior</span>
+            <span class="text-lg font-times">{{ facade.name }}</span>
             <p class="text-[10px] font-nunito">VIEW GALLERY</p>
           </div>
         </div>
@@ -39,27 +70,14 @@
       <div class="flex-row justify-between block px-16 pt-10 md:flex">
         <div class="w-full md:w-3/4">
           <p class="font-bold text-[#38544c] pb-6" data-aos="fade-up">
-            GARBERA CLUSTER<span class="font-thin"> CONCPET</span>
+            <span>{{ cluster.data.name }}</span
+            ><span class="font-thin"> CONCEPT</span>
           </p>
-          <p
-            class="md:w-[80%] w-full leading-[25px] text-[12px] font-[490]"
+          <div
+            class="md:w-[80%] w-full leading-[25px] text-[12px] font-[490] no-tailwind"
             data-aos="fade-up"
-          >
-            Garbera cluster di Greenland menawarkan hunian praktis dengan luas
-            80m2 yang dirancang secara harmonis dan memberikan kesan lapang
-            serta multi fungsi. Penghuni Garbera cluster adalah mereka yang
-            memiliki jiwa aktif dalam kesehariannya, yakni seorang eksekutif
-            atau pengusaha muda yang mempersiapkan masa depannya untuk
-            berekspansi hingga menjadi keluarga kecil dengan 1-2 orang anak.
-            <br />
-            <br />
-            Bentuk facade yang menggunakan warna unsur kayu sangat menyatu
-            dengan lingkungan Greenland yang mengedepankan kehidupan alam hijau.
-            Dengan guarded gate cluster, hunian di Garbera cluster sangat aman
-            meski tanpa dilengkapi oleh pagar. Selain itu kehidupan bersosial
-            dengan sesama dapat terjalin dengan baik sembari tetap memiliki
-            privasi yang eksklusif.
-          </p>
+            v-html="cluster.data.contents"
+          ></div>
         </div>
         <div class="w-full mt-8 md:w-1/4 md:mt-0">
           <p class="font-thin text-[#38544c] md:pb-6 pb-8" data-aos="fade-down">
@@ -67,36 +85,24 @@
           </p>
           <ul class="text-xs font-bold text-[#38544c]" data-aos="fade-down">
             <li class="flex mb-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="#000"
-                class="bi bi-filetype-pdf me-2"
-                viewBox="0 0 16 16"
+              <a
+                :href="`${config.public.dirUrl}/${cluster.data.plan_map}`"
+                target="_blank"
+                class="flex items-center justify-center gap-2"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803q.43 0 .732-.173.305-.175.463-.474a1.4 1.4 0 0 0 .161-.677q0-.375-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333v3.999h1.46q.602 0 .998-.237a1.45 1.45 0 0 0 .595-.689q.196-.45.196-1.084 0-.63-.196-1.075a1.43 1.43 0 0 0-.589-.68q-.396-.234-1.005-.234zm.791.645h.563q.371 0 .609.152a.9.9 0 0 1 .354.454q.118.302.118.753a2.3 2.3 0 0 1-.068.592 1.1 1.1 0 0 1-.196.422.8.8 0 0 1-.334.252 1.3 1.3 0 0 1-.483.082h-.563zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638z"
-                />
-              </svg>
-              Garbera Floor Plan
+                <IconBiFiletypePdf width="18" height="18" />
+                Garbera Plan Map
+              </a>
             </li>
             <li class="flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="#000"
-                class="bi bi-filetype-pdf me-2"
-                viewBox="0 0 16 16"
+              <a
+                :href="`${config.public.dirUrl}/${cluster.data.floor_map}`"
+                target="_blank"
+                class="flex items-center justify-center gap-2"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803q.43 0 .732-.173.305-.175.463-.474a1.4 1.4 0 0 0 .161-.677q0-.375-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333v3.999h1.46q.602 0 .998-.237a1.45 1.45 0 0 0 .595-.689q.196-.45.196-1.084 0-.63-.196-1.075a1.43 1.43 0 0 0-.589-.68q-.396-.234-1.005-.234zm.791.645h.563q.371 0 .609.152a.9.9 0 0 1 .354.454q.118.302.118.753a2.3 2.3 0 0 1-.068.592 1.1 1.1 0 0 1-.196.422.8.8 0 0 1-.334.252 1.3 1.3 0 0 1-.483.082h-.563zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638z"
-                />
-              </svg>
-              Garbera Floor Map
+                <IconBiFiletypePdf width="18" height="18" />
+                Garbera Floor Map
+              </a>
             </li>
           </ul>
         </div>
@@ -104,50 +110,21 @@
 
       <div class="flex justify-end px-16 py-28">
         <div class="text-center">
-          <button
-            class="flex items-center pt-5 mx-auto btn-visit-black"
+          <NuxtLink
+            href="/clusters"
+            class="flex items-center gap-2 pt-5 mx-auto btn-visit-black"
             data-aos="fade-up"
             data-aos-delay="300"
           >
             <span class="text-xs tracking-wider text">BACK TO CLUSTERS</span>
             <span class="circle">
               <span class="arrow">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-arrow-right"
-                  viewBox="0 0 24 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M1 8a.5.5 0 0 1 .5-.5h18.793l-3.146-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L20.293 8.5H1.5A.5.5 0 0 1 1 8z"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  />
-                  <path d="M18 8l2-2v4l-2-2z" fill="currentColor" />
-                </svg>
+                <IconBiArrowRight width="20" height="20" />
               </span>
             </span>
-          </button>
+          </NuxtLink>
         </div>
       </div>
     </section>
   </div>
 </template>
-<style scoped>
-.bg-blend {
-  background: linear-gradient(
-      to top,
-      rgba(248, 244, 236, 0.9),
-      rgba(248, 244, 236, 0.9),
-      rgba(248, 244, 236, 0.8),
-      rgba(248, 244, 236, 0.7),
-      rgba(248, 244, 236, 0.6),
-      rgba(248, 244, 236, 0.6)
-    ),
-    url("/img/detaiLBg.png");
-  background-size: cover;
-}
-</style>
--- pages/clusters/index.vue
+++ pages/clusters/index.vue
@@ -1,15 +1,19 @@
 <script setup>
-const { showClusterGalery } = useClusterImageGalery()
-const settings = useDataSettings()
-const config = useRuntimeConfig()
-const { data: clusters, status, error, refresh } = await useFetch(`${config.public.apiUrl}/clusters`, {
-    pick: ["data"],
-    key: "api-clusters"
+const settings = useDataSettings();
+const config = useRuntimeConfig();
+const {
+  data: clusters,
+  status,
+  error,
+  refresh,
+} = await useFetch(`${config.public.apiUrl}/clusters`, {
+  pick: ["data"],
+  key: "api-clusters",
 });
 useHead({
-    style: [
-        {
-            innerHTML: `
+  style: [
+    {
+      innerHTML: `
       .bg-blend {
         background: linear-gradient(to top,
         rgba(248, 244, 236, 0.9),
@@ -22,126 +26,153 @@ useHead({
         url("${config.public.dirUrl}/images/static/${settings.value.data.hero_cluster_image_1}");
         background-size: cover;
         }
-      `
-        }
-    ],
-    script: [
-        {
-            src: "https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js",
-        },
-    ],
+      `,
+    },
+  ],
+  script: [
+    {
+      src: "https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js",
+    },
+  ],
 });
 </script>
 <template>
-    <div>
-        <section id="section0"
-            class="bg-cover bg-center w-full bg-blend md:h-[400px] h-[200px] flex justify-center items-end md:pb-[5rem] pb-6">
-            <div class="text-center">
-                <h2 class="text-thin font-nunito text-[14px] text-black">
-                    {{ settings.data.hero_cluster_title_1 }}
-                </h2>
-                <h1 class="text-size-main font-times text-[#38544c]">
-                    {{ settings.data.hero_cluster_info_1 }}
-                </h1>
-            </div>
-        </section>
+  <div>
+    <section
+      id="section0"
+      class="bg-cover bg-center w-full bg-blend md:h-[400px] h-[200px] flex justify-center items-end md:pb-[5rem] pb-6"
+    >
+      <div class="text-center">
+        <h2 class="text-thin font-nunito text-[14px] text-black">
+          {{ settings.data.hero_cluster_title_1 }}
+        </h2>
+        <h1 class="text-size-main font-times text-[#38544c]">
+          {{ settings.data.hero_cluster_info_1 }}
+        </h1>
+      </div>
+    </section>
 
-        <section id="section1" class="bg-[#F8F4EC] w-full justify-center pt-6 md:pt-0">
-            <div class="w-full">
-                <div class="flex flex-wrap gap-4 w-[90%] mx-auto">
-                    <div class="relative group" data-aos="zoom-in" v-for="(cluster) in clusters.data.data"
-                        :key="cluster.id">
-                        <img :src="`${config.public.dirUrl}/${cluster.image}`" alt="Photo 1"
-                            class="object-cover w-full h-auto" />
-                        <div
-                            class="absolute inset-0 flex flex-col justify-end text-white transition-all duration-300 bg-black bg-opacity-20 group-hover:bg-white group-hover:bg-opacity-50">
-                            <h3
-                                class="text-4xl font-times capitalize ml-5 group-hover:text-[#38544c] transition-all duration-300">
-                                {{ cluster.name }}
-                            </h3>
-                            <button
-                                @click="showClusterGalery({ images: [cluster.image, cluster.image2, ...cluster.facade.map(facade => facade.image)], description: cluster.name })"
-                                class="mt-1 text-white mb-5 ml-5 group-hover:text-[#38544c] transition-all duration-300 text-[10px] font-bold">VIEW
-                                GALLERY</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="pt-8 pb-10 text-center">
-                    <button class="flex items-center pt-5 mx-auto btn-visit-black" data-aos="fade-up"
-                        data-aos-delay="300">
-                        <span class="text-xs tracking-wider text">SCHEDULE A VISIT</span>
-                        <span class="circle">
-                            <span class="arrow">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor"
-                                    class="bi bi-arrow-right" viewBox="0 0 24 16">
-                                    <path fill-rule="evenodd"
-                                        d="M1 8a.5.5 0 0 1 .5-.5h18.793l-3.146-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L20.293 8.5H1.5A.5.5 0 0 1 1 8z"
-                                        stroke="currentColor" stroke-width="2" />
-                                    <path d="M18 8l2-2v4l-2-2z" fill="currentColor" />
-                                </svg>
-                            </span>
-                        </span>
-                    </button>
-                </div>
+    <section
+      id="section1"
+      class="bg-[#F8F4EC] w-full justify-center pt-6 md:pt-0"
+    >
+      <div class="flex flex-wrap gap-4 w-[90%] mx-auto">
+        <div
+          class="relative group w-full sm:max-w-[48%] md:max-w-[31%] lg:max-w-[23%]"
+          data-aos="zoom-in"
+          v-for="cluster in clusters.data.data"
+          :key="cluster.id"
+        >
+          <img
+            :src="`${config.public.dirUrl}/${cluster.image}`"
+            alt="Photo 1"
+            class="object-cover w-full h-auto"
+          />
+          <div
+            class="absolute inset-0 flex flex-col justify-end p-5 text-white transition-all duration-300 bg-black bg-opacity-20 group-hover:bg-white group-hover:bg-opacity-50"
+          >
+            <h3
+              class="text-4xl font-times capitalize group-hover:text-[#38544c] transition-all duration-300 mb-3"
+            >
+              {{ cluster.name }}
+            </h3>
+            <div class="flex items-center justify-start">
+              <NuxtLink
+                :to="`/clusters/${cluster.id}`"
+                class="text-white group-hover:text-[#38544c] transition-all duration-300 text-[10px] font-bold"
+                >VIEW GALLERY</NuxtLink
+              >
             </div>
-        </section>
-    </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="pt-8 pb-10 text-center">
+        <button
+          class="flex items-center pt-5 mx-auto btn-visit-black"
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
+          <span class="text-xs tracking-wider text">SCHEDULE A VISIT</span>
+          <span class="circle">
+            <span class="arrow">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right"
+                viewBox="0 0 24 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h18.793l-3.146-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L20.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <path d="M18 8l2-2v4l-2-2z" fill="currentColor" />
+              </svg>
+            </span>
+          </span>
+        </button>
+      </div>
+    </section>
+  </div>
 </template>
 <style scoped>
 .btn-visit-black {
-    display: inline-flex;
-    align-items: center;
-    background-color: transparent;
-    border: none;
-    color: #000;
-    font-size: 16px;
-    font-weight: bold;
-    text-transform: uppercase;
-    cursor: pointer;
-    position: relative;
+  display: inline-flex;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  color: #000;
+  font-size: 16px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  position: relative;
 }
 
 .btn-visit-black .text {
-    margin-right: 10px;
+  margin-right: 10px;
 }
 
 .btn-visit-black .circle {
-    display: inline-block;
-    border: 2px solid #000;
-    border-radius: 50%;
-    width: 30px;
-    height: 30px;
-    position: relative;
-    transition: 0.4s ease-in-out;
+  display: inline-block;
+  border: 2px solid #000;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  position: relative;
+  transition: 0.4s ease-in-out;
 }
 
 .btn-visit-black .arrow {
-    position: absolute;
-    top: 50%;
-    left: -8px;
-    transform: translateY(-50%);
-    transition: 0.4s ease-in-out;
+  position: absolute;
+  top: 50%;
+  left: -8px;
+  transform: translateY(-50%);
+  transition: 0.4s ease-in-out;
 }
 
 .btn-visit-black:hover .arrow {
-    left: 5px;
+  left: 5px;
 }
 
 .btn-visit-black:hover .circle {
-    background-color: #000;
+  background-color: #000;
 }
 
 .btn-visit-black:hover .arrow {
-    color: white;
+  color: white;
 }
 
 .btn-visit-black {
-    transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
 }
 
 .btn-visit-black:hover {
-    transform: scale(1.1);
-    color: #000;
+  transform: scale(1.1);
+  color: #000;
 }
 </style>
