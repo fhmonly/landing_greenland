d844bd8cf62aacef8c6848f78d0a5ac4f4a4e164
ibnudrift <ibnudrift@gmail.com>
2024-10-31 15:36:20 +0700

    revision promo n news

--- pages/promo/index.vue
+++ pages/promo/index.vue
@@ -19,14 +19,14 @@
             class="bg-white text-[#38544c] w-[350px] px-12 py-8 rounded-lg flex flex-col justify-between hover:bg-[#38544c] hover:text-white"
             data-aos="fade-up" data-aos-duration="300" v-for="promo in promos.data.data" :key="promo">
             <div>
-              <p class="text-[10px] font-semibold">NEWS - {{ $dayjs(promo.date_input).locale("id").fromNow() }}</p>
+              <p class="text-[10px] font-semibold">PROMO - {{ $dayjs(promo.date_input).locale("id").fromNow() }}</p>
               <h3 class="text-xl font-base leading-[30px] mt-2">
                 {{ promo.title }}
               </h3>
             </div>
             <div class="mt-auto">
               <hr class="my-3 border-[#BFCEC9FF]" />
-              <NuxtLink :to="`/promos/${promo.topik_id}`" class="mb-4 text-[10px] font-bold">READ MORE</NuxtLink>
+              <NuxtLink :to="`/promo/${promo.id}`" class="mb-4 text-[10px] font-bold">READ MORE</NuxtLink>
             </div>
           </div>
         </div>
